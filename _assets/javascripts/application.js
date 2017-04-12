f2f42952748defbc4d0e11862cd652e176ab0bd9
hicnblog <hnugroho@indo-soft.com>
2017-04-12 11:00:18 +0700

    update js for delete turbolinks

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,43 +1,5 @@
 //= require_self
 
-function on_init_load(){
-    var check_table_markdown = $('p#table_markdown_show').length;
-    var check_diary_on = $('p#diary_on_show').length;
-
-    if (check_table_markdown !== 0) {
-        $("code").css('cursor', 'pointer');
-        $("code").click(function() {
-            var html_val = $(this).html();
-
-            var ClipboardHelper = {
-                copyElement: function($element) {
-                    this.copyText($element.text())
-                },
-                copyText: function(text) { // Linebreaks with \n
-                    var $tempInput = $("<textarea>");
-                    $("body").append($tempInput);
-                    $tempInput.val(text).select();
-                    document.execCommand("copy");
-                    $tempInput.remove();
-                    this.copyFinished('Copied!!!', 1500);
-                },
-                copyFinished: function(textCopied, timeout) {
-                    Materialize.toast(textCopied, timeout);
-                }
-            };
-
-            ClipboardHelper.copyText(html_val);
-
-        }); // on click
-
-        $("code").tooltip({
-            delay: 50,
-            tooltip: "Klik untuk copy text emoji"
-        });
-        
-    }
-}
-
 $(document).on('turbolinks:load', function () {
   // Initialize fluidbox
   $('.fluidbox-trigger').fluidbox();
@@ -57,7 +19,5 @@ $(document).on('turbolinks:load', function () {
     reset: false,
     viewFactor: 0
   });
-  on_init_load();
 });
-
 window.sr = ScrollReveal({ reset: true });
