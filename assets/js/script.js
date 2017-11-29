58db7addc6a7abc3479da8b0c476b849b6b9d169
hicnblog <hnugroho@indo-soft.com>
2017-11-29 11:11:45 +0700

    Adding feature to change dark or light on website display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 (function($) {
     'use strict';
+    var get_origin = window.location;
     function on_init_load(){
         var check_table_markdown = $('p#table_markdown_show').length;
         var check_diary_on = $('p#diary_on_show').length;
@@ -27,7 +28,6 @@
                 };
 
                 ClipboardHelper.copyText(html_val);
-
             }); // on click
 
             $("code").tooltip({
@@ -41,7 +41,26 @@
         
     }
 
+    function switcher_display_website(){
+        var elem    = document.querySelector('.hicnblog-js-switch');
+        var hd_hd = localStorage.getItem("hicnblog_display");
+        if(hd_hd !== null && hd_hd !== 'false'){
+            elem.checked = true;
+            var switchery = new Switchery(elem, { color: '#ffffff', secondaryColor: '#000000', jackColor: '#f7f7f7', jackSecondaryColor: '#424242' });
+            $('head').append('<link rel="stylesheet" href="'+get_origin.origin+'/assets/css/light/main.css">');
+        } else {
+            var switchery = new Switchery(elem, { color: '#ffffff', secondaryColor: '#000000', jackColor: '#f7f7f7', jackSecondaryColor: '#424242' });
+        }
+        elem.onchange = function() {
+          localStorage.setItem('hicnblog_display', elem.checked);
+          setTimeout(function(){
+            location.reload();
+          }, 1000);
+        };
+    }
+
 $(document).ready(function(){
     on_init_load();
+    switcher_display_website();
 });
 })(jQuery);
